0ec64a45f679af815737d8a73074d85d22c0685e
apfp77 <apfp77@naver.com>
2023-05-24 18:14:27 +0900

    test: server_tmp socket ~ send

--- server_tmp.cpp
+++ server_tmp.cpp
@@ -38,9 +38,6 @@ int main(int argc, char const **argv)
 	serverAddress.ai_flags = AI_PASSIVE;	 
 	getaddrinfo(NULL, argv[1], &serverAddress, &_servinfo);
 
-	std::cout << _servinfo->ai_protocol << '\n';
-
-
 	serverSocket = socket(_servinfo->ai_family, _servinfo->ai_socktype, _servinfo->ai_protocol);
 	if (serverSocket == -1) {
 		perror("Failed to create server socket");
@@ -59,52 +56,53 @@ int main(int argc, char const **argv)
 	// Initialize the fds array
 	fds[0].fd = serverSocket;
 	fds[0].events = POLLIN;
-	for (i = 1; i < MAX_CLIENTS + 1; ++i) {
+	for (i = 1; i < MAX_CLIENTS + 1; ++i)
 		fds[i].fd = -1;
-	}
 
 	nfds = 1; // Initial number of file descriptors to monitor
 
-	while (1) {
+	while (1) 
+	{
 		// Wait for events on file descriptors
 		ret = poll(fds, nfds, -1);
-		if (ret == -1) {
+		if (ret == -1) 
+		{
 			perror("Error in poll");
 			break;
 		}
 
 		// Check for events on server socket
 		if (fds[0].revents & POLLIN) 
-				{
+		{
 			// Accept incoming connection
 			clientAddressLength = sizeof(clientAddress);
 			int newClientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddressLength);
 			if (newClientSocket == -1)
-						{
+			{
 				perror("Failed to accept client connection");
 				continue;
 			}
 
 			// Add new client socket to the fds array
 			for (i = 1; i < MAX_CLIENTS + 1; ++i) 
-						{
+			{
 				if (fds[i].fd == -1) 
-								{
-									fds[i].fd = newClientSocket;
-									fds[i].events = POLLIN;
-									break;
+				{
+					fds[i].fd = newClientSocket;
+					fds[i].events = POLLIN;
+					break;
 				}
 			}
 			if (i == MAX_CLIENTS + 1) 
-						{
+			{
 				fprintf(stderr, "Too many clients, connection rejected\n");
 				close(newClientSocket);
 			} 
-						else 
-						{
-							if (i + 1 > nfds) 
-									nfds = i + 1;
-							printf("New client connected\n");
+			else 
+			{
+				if (i + 1 > nfds) 
+						nfds = i + 1;
+				printf("New client connected\n");
 			}
 		}
 
@@ -126,9 +124,7 @@ int main(int argc, char const **argv)
 					fds[i].fd = -1;
 				}
 				else 
-				{
 					printf("%s\n", buffer);
-				}
 			}
 		}
 	}
